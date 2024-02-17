6c91ac0cb4da4348987f9950993f61a4ffb15729
MinasFakhori <minasfakhori1@outlook.com>
2024-02-17 22:42:20 +0000

    Made it on load

--- API/get.py
+++ API/get.py
@@ -1,11 +1,8 @@
-import requests
+import face_recognition
+known_image = face_recognition.load_image_file("biden.jpg")
+unknown_image = face_recognition.load_image_file("unknown.jpg")
 
-def get_dev():
-    res= requests.get(
-        "https://api.apis.guru/v2/list.json",
-    )
-    
-    return res
+biden_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-
-print(get_dev())
+results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
--- Website/index.js
+++ Website/index.js
@@ -1,30 +1,37 @@
-function games(num_games) {
-    games = []   
+// on window load
+window.onload = function() {
+  
+  let randomGameLink = generateRandomGameLink();
+  generateQRCode(randomGameLink);
 
+  // Set a timer to redirect after 3 mintues
+  setTimeout(function() {
+      window.location.href = "invalid.html";
+  }, 180000);
+
+}
+
+
+function games_getter(num_games) {
+    games = [];
     for (let i = 0; i < num_games; i++) {
-        games.push("https://laytongb.github.io/hackathon-2024-proj/" + i );
+        games.push("https://laytongb.github.io/hackathon-2024-proj/" +  i);
     }
     return games;
 
 }
 
 
+
 function generateRandomGameLink() {
-    let num_games = 1; // Number of games
-    let gamesArray = games(num_games);
-    let randomIndex = Math.floor(Math.random() * gamesArray.length);
+    let num_games = 2; 
+    let gamesArray = games_getter(num_games);
+    let randomIndex = Math.floor(Math.random() * num_games);
+
     return gamesArray[randomIndex];
 }
 
-document.querySelector("#generateBtn").addEventListener("click", () => {
-    let randomGameLink = generateRandomGameLink();
-    generateQRCode(randomGameLink);
 
-    // Set a timer to redirect after 3 mintues
-    setTimeout(function() {
-        window.location.href = "invalid.html";
-    }, 180000);
-});
 
 function generateQRCode(text) {
   let qrCodeContainer = document.getElementById('qrCodeContainer');
